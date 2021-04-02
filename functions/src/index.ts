4898823480db2c966c3200f040ed11a4d216a2b9
Louis DESPLANCHE <l.desplanche@gmail.com>
2021-04-02 19:28:31 +0200

    Add add point notification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,8 @@ import * as admin from "firebase-admin";
 import * as https from "https";
 import * as sxml from "sxml";
 
-
 admin.initializeApp();
+
 
 exports.ssoLogin = functions.https.onRequest(async (req, res) => {
     const fncUrl = `https://${req.header("host")}/${process.env.FUNCTION_TARGET}${req.path}`;
@@ -43,7 +43,6 @@ exports.ssoLogin = functions.https.onRequest(async (req, res) => {
 
     const xmlS = new sxml.XML(result);
 
-    console.log(result);
 
     const user: string = xmlS.get("cas:authenticationSuccess").front()
         .get("cas:user").front().getValue().toLowerCase();
@@ -52,10 +51,39 @@ exports.ssoLogin = functions.https.onRequest(async (req, res) => {
 
     const token = await admin.auth().createCustomToken(user);
 
-    console.log(`Token: ${token}`);
-
     res.json({
         user: user,
         token: token,
     });
 });
+
+exports.onUserEdition = functions.firestore
+    .document("users/{docId}")
+    .onWrite(async (change, context) => {
+        const prevData = change.before.data() ?? {"points": 0};
+        const nextData = change.after.data() ?? {"points": 0};
+        if (nextData["point"] > prevData["point"]) {
+            const diff = nextData["point"] - prevData["point"];
+            console.log("Send message to : " + change.after.id);
+            await admin.messaging().sendToTopic(
+                "/topics/user" + change.after.id,
+                {
+                    data: {},
+                    notification: {
+                        title: "Tu as gagné des points !",
+                        body: `Tu as gagné ${diff} points !\n` +
+                            "Zeus est très impressionné !",
+                        sound: "default",
+                        // badge: '1'
+                    },
+                },
+                {
+                    collapseKey: "point",
+                    contentAvailable: true,
+                    priority: "high",
+                    timeToLive: 60 * 60 * 24,
+                },
+            );
+        }
+    });
+
