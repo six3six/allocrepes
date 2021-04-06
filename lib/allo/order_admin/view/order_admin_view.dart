4f58741ffe9ed2465cf128ea8e5a6e42e1f6ebf2
Louis DESPLANCHE <l.desplanche@gmail.com>
2021-04-06 22:33:21 +0200

    Passage en beta

--- lib/allo/order_admin/view/order_admin_view.dart
+++ lib/allo/order_admin/view/order_admin_view.dart
@@ -100,18 +100,22 @@ class OrderAdminView extends StatelessWidget {
       headerBuilder: (BuildContext context, bool isExpanded) {
         if (isExpanded)
           return ListTile(
-            title: Text("${order.owner} - ${order.createdAt.toLocal().toString().split(".")[0].substring(0, 16)}",),
+            title: Text(
+              "${order.owner} - ${order.createdAt.toLocal().toString().split(".")[0].substring(0, 16)}",
+            ),
             tileColor: PlaceUtils.placeToColor(order.place),
           );
         else
           return ListTile(
-            title: Text("${order.owner} - ${order.createdAt.toLocal().toString().split(".")[0].substring(0, 16)}",),
+            title: Text(
+              "${order.owner} - ${order.createdAt.toLocal().toString().split(".")[0].substring(0, 16)}",
+            ),
             tileColor: PlaceUtils.placeToColor(order.place),
             selectedTileColor: PlaceUtils.placeToColor(order.place),
             subtitle: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: order.articles
-                  .map((article) => _ArticleToProductLabel(
+                  .map<Widget>((article) => _ArticleToProductLabel(
                         article: article,
                       ))
                   .toList(),
@@ -226,47 +230,59 @@ class _OrderCompleteView extends StatelessWidget {
 
     return ListTile(
       title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Nom :",
-            style: theme.textTheme.caption,
-          ),
-          _UserLabel(
-            userId: order.owner,
-            classe: true,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Adresse :",
-            style: theme.textTheme.caption,
-          ),
-          Text(
-            "${PlaceUtils.placeToString(order.place)}  -  ${order.room}",
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Commande :",
-            style: theme.textTheme.caption,
-          ),
-        ]
-          ..addAll(order.articles
-              .map(
-                (article) => _ArticleToProductLabel(
-                  article: article,
-                ),
-              )
-              .toList())
-          ..add(
-            _StateSelector(
-              order: order,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Nom :",
+              style: theme.textTheme.caption,
             ),
-          ),
-      ),
+            _UserLabel(
+              userId: order.owner,
+              classe: true,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Adresse :",
+              style: theme.textTheme.caption,
+            ),
+            Text(
+              "${PlaceUtils.placeToString(order.place)}  -  ${order.room}",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Commande :",
+              style: theme.textTheme.caption,
+            ),
+          ]
+            ..addAll(order.articles
+                .map(
+                  (article) => _ArticleToProductLabel(
+                    article: article,
+                  ),
+                )
+                .toList())
+            ..add(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10),
+                  Text(
+                    "Commentaire :",
+                    style: theme.textTheme.caption,
+                  ),
+                  Text(order.message),
+                ],
+              ),
+            )
+            ..add(
+              _StateSelector(
+                order: order,
+              ),
+            )),
     );
   }
 }
