a529ea1df5e882bcc68f71bde5746128e1487875
Louis Desplanche <louis.desplanche@edu.esiee.fr>
2021-04-25 23:30:26 +0200

    Add crashlytics log

--- lib/app_observer.dart
+++ lib/app_observer.dart
@@ -1,9 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 
 class AppObserver extends BlocObserver {
   @override
   void onChange(BlocBase cubit, Change change) {
     print('${cubit.runtimeType} $change');
+    if (!kIsWeb) {
+      FirebaseCrashlytics.instance.log('${cubit.runtimeType} $change');
+    }
     super.onChange(cubit, change);
   }
 }
