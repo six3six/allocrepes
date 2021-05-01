c7efd8dfb673240be5f6870730d101f51ff021fd
Louis DESPLANCHE <l.desplanche@gmail.com>
2021-05-01 09:38:04 +0200

    Ajout des paramètres de transparence

--- lib/app.dart
+++ lib/app.dart
@@ -3,11 +3,14 @@ import 'dart:io';
 import 'package:allocrepes/allo/oder_new/view/order_new_page.dart';
 import 'package:allocrepes/splash/splash.dart';
 import 'package:allocrepes/theme.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:order_repository/order_repository_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -115,11 +118,33 @@ class _AppViewState extends State<AppView> {
               case AuthenticationStatus.authenticated:
                 print('authenticated');
                 if (!kIsWeb) {
-                  print('subscribeToTopic(user${state.user.id})');
-                  FirebaseMessaging.instance
-                      .subscribeToTopic('user${state.user.id}');
+                  try {
+                    print('subscribeToTopic(user${state.user.id})');
+                    FirebaseMessaging.instance
+                        .subscribeToTopic('user${state.user.id}');
+                  } catch (exception, stack) {
+                    FirebaseCrashlytics.instance.recordError(exception, stack);
+                  }
+                  try {
+                    AppTrackingTransparency.requestTrackingAuthorization()
+                        .then((status) {
+                      switch (status) {
+                        case TrackingStatus.notDetermined:
+                        case TrackingStatus.restricted:
+                        case TrackingStatus.denied:
+                          break;
+                        case TrackingStatus.authorized:
+                        case TrackingStatus.notSupported:
+                          FirebaseCrashlytics.instance
+                              .setUserIdentifier(state.user.id);
+                          break;
+                      }
+                    });
+                    // If the system can show an authorization request dialog
 
-                  FirebaseCrashlytics.instance.setUserIdentifier(state.user.id);
+                  } catch (e, stack) {
+                    FirebaseCrashlytics.instance.recordError(e, stack);
+                  }
                 }
                 RepositoryProvider.of<OrderRepositoryFirestore>(context)
                     .changeUser(state.user.id);
