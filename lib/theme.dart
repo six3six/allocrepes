03018a99ef86a69e8fce47ccac4ad4ee99a45d20
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2023-05-17 14:05:30 +0200

    Changement de la palette de couleur

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0xffa58c71);
+const Color primaryColor = Color(0xffC69C6D);
 const Color primaryColorLight = Color(0xffF3EFCC);
 const Color primaryColorDark = Color(0xff42210B);
 
 final theme = ThemeData(
 // Define the default brightness and colors.
   brightness: Brightness.light,
-  colorSchemeSeed: primaryColorLight,
+  colorSchemeSeed: primaryColor,
   useMaterial3: true,
 
   visualDensity: VisualDensity.adaptivePlatformDensity,
