904aaec168f2cf0e5c598aa37ff6564f3b024015
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2023-05-24 11:43:15 +0200

    Ajout des CI XCode

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+
+# Install Flutter
+brew install --cask flutter
+
+# Run Flutter doctor
+flutter doctor
+
+# Enable macos
+flutter config --enable-macos-desktop
+
+# Get packages
+flutter packages get
+
+# Update generated files
+flutter pub run build_runner build
+
+# Build ios app
+flutter build ios --no-codesign
